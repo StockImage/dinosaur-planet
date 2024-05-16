225c9de69ba7fd4a93b61c30fda5c2776d09d417
Chris Richardson <chris.richardson1801@gmail.com>
2024-05-16 20:53:45 +0930

    Decomp segment_11EF0.c

--- src/segment_11EF0.c
+++ src/segment_11EF0.c
@@ -1,3 +1,21 @@
 #include "common.h"
-
+#if 0
 #pragma GLOBAL_ASM("asm/nonmatchings/segment_11EF0/NOTosContSetch.s")
+#else
+
+extern u8 __osContLastCmd; // Could be s8 
+
+s32 NOTosContSetch(OSMesgQueue* arg0, s32 arg1) {
+    s32 sp1C;
+
+    __osSiGetAccess();
+    __osSiRawStartDma(1, arg1);
+    osRecvMesg(arg0, NULL, 1);
+    sp1C = __osSiRawStartDma(0, arg1);
+    osRecvMesg(arg0, NULL, 1);
+    __osContLastCmd = 0xFE;
+    __osSiRelAccess();
+    return sp1C;
+}
+
+#endif
